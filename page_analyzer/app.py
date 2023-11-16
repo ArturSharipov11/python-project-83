3d93c2af3180aade50a3ca1ea84134532dd0fa09
artur sharipov <sharipoffartur@yandex.ru>
2023-11-16 23:01:26 +0500

    step 2, process

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,10 +1,15 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/two')
+def two():
+    return render_template('two.html')
 
 
 if __name__ == "__main__":
